e26a5118dd2de57dd0b1595f8a827722b0d9a4e3
dronarioo <vova.lagola@bk.ru>
2022-12-12 12:39:08 +0300

    Создали методы get

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ class Rectangle:
     def area(self):
         return self._a * self._b
 
+    def get_height(self):
+        return self._a
+
+    def get_width(self):
+        return self._b
+
 
 main_rect = Rectangle(5, 4)
 print(main_rect.area())
